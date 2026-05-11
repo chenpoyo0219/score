f895dabcfa3daeab1ef326b0b8743fd0af485ffa
User <a0983876136@gmail.com>
2026-05-11 11:07:11 +0800

    Update score.py

--- score.py
+++ score.py
@@ -1,29 +1,17 @@
-
-
 scores = input().split()
-
-count = 0
-
 nums = [int(s) for s in scores]   # 把字串轉成整數列表
 
+count = 0
+total = 0
+
 for s in nums:
+    total += s
     if s < 60:
         count += 1
+
+average = total / len(nums)
 
 print("不及格人數:", count)
 print("最大值:", max(nums))
 print("最小值:", min(nums))
-=======
-total = 0
-
-for s in scores:
-    score = int(s)
-    total += score
-    if score < 60:
-        count += 1
-
-average = total / len(scores)
-
-print("不及格人數:", count)
 print("平均分數:", average)
-
