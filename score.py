27547a05139728e2359092edcfc7d1a3726654c1
chenpoyo0219 <yoyo0983698150@gmail.com>
2026-05-11 02:44:59 +0000

    Add max min

--- score.py
+++ score.py
@@ -1,9 +1,12 @@
 scores = input().split()
 
 count = 0
+nums = [int(s) for s in scores]   # 把字串轉成整數列表
 
-for s in scores:
-    if int(s) < 60:
-        count=count+1
+for s in nums:
+    if s < 60:
+        count += 1
 
-print(count)
+print("不及格人數:", count)
+print("最大值:", max(nums))
+print("最小值:", min(nums))
